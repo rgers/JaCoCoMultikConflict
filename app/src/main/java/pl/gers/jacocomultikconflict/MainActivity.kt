72b367302d54b39e3008e2aad54567266004eb82
Radoslaw Gers <ger1wz@bosch.com>
2023-04-25 10:33:13 +0200

    JaCoCo conflict

---
+++ app/src/main/java/pl/gers/jacocomultikconflict/MainActivity.kt
@@ -0,0 +1,12 @@
+package pl.gers.jacocomultikconflict
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.gers.jacocomultikconflict.R.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
---
+++ app/src/test/java/pl/gers/jacocomultikconflict/ExampleUnitTest.kt
@@ -0,0 +1,20 @@
+package pl.gers.jacocomultikconflict
+
+import org.jetbrains.kotlinx.multik.api.linalg.inv
+import org.jetbrains.kotlinx.multik.api.mk
+import org.jetbrains.kotlinx.multik.api.ndarray
+import org.jetbrains.kotlinx.multik.ndarray.operations.stack
+import org.junit.Test
+
+/**
+ * A unit test that fails with java verifyError when run with JaCoCo
+ */
+class ExampleUnitTest {
+    @Test
+    fun multikest() {
+        val top = mk.ndarray(doubleArrayOf(1.0, 2.0, 3.0, 4.0, 5.0))
+        val bottom = mk.ndarray(doubleArrayOf(6.0, 7.0, 8.0, 9.0, 10.0))
+        val matrix = mk.stack(top, bottom)
+        mk.linalg.inv(matrix)
+    }
+}
